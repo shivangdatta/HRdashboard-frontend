160874dd1fc8603c221a2bf3bba8658e06f87255
shivang datta <shivangdatta@gmail.com>
2024-07-31 17:37:33 +0530

    spinnder bug fix

--- src/components/UniqueSpinner.js
+++ src/components/UniqueSpinner.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const UniqueSpinner = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-black">
+    <div className="flex justify-center items-center min-h-screen bg-black">
       <div className="relative w-16 h-16">
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="relative w-full h-full">
             <div className="absolute w-12 h-12 border-4 border-t-4 border-gray-200 border-solid rounded-full animate-spin" style={{ borderTopColor: '#F97316' }}></div>
-            <div className="absolute w-12 h-12 border-4 border-t-4 border-gray-200 border-solid rounded-full animate-spin animate-delay-150" style={{ borderTopColor: '#FECACA' }}></div>
+            {/* <div className="absolute w-12 h-12 border-4 border-t-4 border-gray-200 border-solid rounded-full animate-spin animate-delay-150" style={{ borderTopColor: '#FECACA' }}></div> */}
           </div>
         </div>
         <div className="absolute inset-0 flex items-center justify-center">
